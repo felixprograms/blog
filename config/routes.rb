2a6d276080d5ee7f5efb3f6d0f048be5eff4985d
felix <felixprograms1205@gmail.com>
2021-06-13 10:46:51 +0800

    InstalledPostGrSQL

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
-  # get '/articles', to: 'articles#index'
-  # get '/articles/new', to: 'articles#new' 
-  # post '/articles', to: 'articles#create'
-  # get '/articles/:id/edit', to: 'articles#edit'
-  # patch '/articles/:id', to: 'articles#update'
-  # delete '/articles/:id', to: 'articles#destroy'
+  resources :articles do
+    resources :comments
+  end
 end
-# #        root GET    /                            articles#index
-# # articles GET    /articles(.:format)          articles#index
-# # new_article GET    /articles/new(.:format)      articles#new
-# #     article GET    /articles/:id(.:format)      articles#show
-# #             POST   /articles(.:format)          articles#create
-# # edit_article GET    /articles/:id/edit(.:format) articles#edit
-# #             PATCH  /articles/:id(.:format)      articles#update
-# #             DELETE /articles/:id(.:format)      articles#destroy
