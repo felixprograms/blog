9e14e10adb5d30e3e27defd44f8e2a478e4a21f6
felix <felixprograms1205@gmail.com>
2021-11-07 10:21:10 +0800

    tic-tac-toe-2

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,1 +1,29 @@
 console.log("hello world")
+let currentPlayer = "X"
+function handleClick(event) {
+    console.log(event.target.dataset.index)
+    gameState[event.target.dataset.index] = currentPlayer
+    draw()
+    if (currentPlayer == "X") {
+        currentPlayer = "0"
+    } else {
+        currentPlayer = "X"
+    }
+}
+
+let gameState = ['0','','X','','','','','','']
+function draw () {
+    document.querySelectorAll('.tictactoe-cell').forEach(cell => {
+        cell.innerHTML = gameState[cell.dataset.index]
+    })
+}
+document.querySelectorAll('.tictactoe-cell').forEach(cell => {
+    draw()
+    cell.addEventListener('click', handleClick)
+})
+
+document.querySelector('#reset-game').addEventListener('click', function() {
+    document.querySelectorAll('.tictactoe-cell').forEach(cell => {
+        cell.innerHTML = ""
+    })
+})
