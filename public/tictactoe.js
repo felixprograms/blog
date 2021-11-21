aaa3ba84885073094ba076ba9c44208ab32d8ab1
felix <felixprograms1205@gmail.com>
2021-11-21 09:08:30 +0800

    tic-tac-toe-winning_condition-3

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,21 +1,57 @@
 console.log("hello world")
+
+let winningIndices = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],  [2,4,6]]
+
 let currentPlayer = "X"
+
+function checkIfPlayerWon(player) {
+    winningIndices.forEach(indices => {
+        if (indices.every(index =>
+            gameState[index] == player
+        )) {
+            document.querySelector('#game-status').innerHTML = `${player} Won`
+
+            draw()
+        }
+    })
+}
+
 function handleClick(event) {
-    console.log(event.target.dataset.index)
-    gameState[event.target.dataset.index] = currentPlayer
+    if (event.target.innerHTML != '') return
+    // console.log(event.target.dataset.index)
+    console.log(event.target.innerHTML)
+    if (document.querySelector('#game-status').innerHTML != `X Won`) {
+        gameState[event.target.dataset.index] = currentPlayer
+    } else {
+        gamestate = ['','','','','','','','','']
+    }
     draw()
     if (currentPlayer == "X") {
         currentPlayer = "0"
     } else {
         currentPlayer = "X"
     }
+    document.querySelector('#game-status').innerHTML = `It's ${currentPlayer}'s turn`
+    if (gameState.every(el => {
+        return el != ''
+    })) {
+        document.querySelector('#game-status').innerHTML = `It's a draw`
+    }
+
+    
+    checkIfPlayerWon("X")
+    checkIfPlayerWon("O")
+
+    
+
 }
 
-let gameState = ['0','','X','','','','','','']
+let gameState = ['','','','','','','','','']
 function draw () {
     document.querySelectorAll('.tictactoe-cell').forEach(cell => {
         cell.innerHTML = gameState[cell.dataset.index]
     })
+    
 }
 document.querySelectorAll('.tictactoe-cell').forEach(cell => {
     draw()
@@ -23,7 +59,6 @@ document.querySelectorAll('.tictactoe-cell').forEach(cell => {
 })
 
 document.querySelector('#reset-game').addEventListener('click', function() {
-    document.querySelectorAll('.tictactoe-cell').forEach(cell => {
-        cell.innerHTML = ""
-    })
-})
+    gameState = ['','','','','','','','','']
+    draw()
+})    
