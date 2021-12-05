811853cd5098238f994abb9afde0d1a7932a2a66
felix <felixprograms1205@gmail.com>
2021-12-05 09:55:48 +0800

    snake-game-2

--- public/snake.js
+++ public/snake.js
@@ -1,6 +1,28 @@
 let index = 0
+let x = 0
+let y = 0
 setInterval(() => {
+    index = x + y*100
     let box = document.querySelector(`[data-index='${index}']`)
     box.classList.add('bg-black')
-    index += 1
 }, 1);
+let audioObj = new Audio('/3.wav') 
+audioObj.volume = 0.3
+window.addEventListener("keyup", function(event) {
+    // const p = document.createElement("p");
+    // p.textContent = `KeyboardEvent: key='${event.key}' | code='${event.code}'`;
+    // document.getElementById("output").appendChild(p)
+    if (event.key == 'ArrowUp'){
+        y -= 1
+    } else if (event.key == 'ArrowDown'){
+        y += 1
+    } else if (event.key == 'ArrowLeft'){
+        x -= 1
+    } else if (event.key == 'ArrowRight'){
+        x += 1
+    }
+
+}, true);
+
+
+
