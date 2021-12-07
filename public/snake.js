8aa69e40f433ec05019058bad191c72d6889a5ac
felix <felixprograms1205@gmail.com>
2021-12-07 20:03:55 +0800

    snake-food

--- public/snake.js
+++ public/snake.js
@@ -3,6 +3,17 @@ let x = 0
 let y = 0
 let snakeLength = 3
 let snakeBody = []
+let snakeFood = []
+
+for (let i = 0; i < 100; i += 1) {
+    let randomIndex = Math.floor(Math.random() * 10000)
+    snakeFood.push(randomIndex)
+    let foodBox = document.querySelector(`[data-index='${randomIndex}']`)
+    foodBox.classList.add('bg-red-900')
+
+}
+
+
 
 setInterval(() => {
     snakeBody.unshift(index)
@@ -10,12 +21,21 @@ setInterval(() => {
         let removedIndex = snakeBody.pop()
         let currentBox = document.querySelector(`[data-index='${removedIndex}']`)
         currentBox.classList.remove('bg-black')
+
+        if (currentBox.classList.contains('bg-red-900')){
+            snakeLength += 1
+            currentBox.classList.remove('bg-red-900')
+
+        }
+
     }
     index += y*100
     index += x
     let nextBox = document.querySelector(`[data-index='${index}']`)
 
     nextBox.classList.add('bg-black')
+
+
 }, 250);
 let audioObj = new Audio('/3.wav') 
 audioObj.volume = 0.3
