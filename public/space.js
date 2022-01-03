cac4273a3fa29249cb713283306e6e1d69f1ffe6
felix <felixprograms1205@gmail.com>
2022-01-03 17:19:15 +0800

    commit

--- public/space.js
+++ public/space.js
@@ -1,36 +1,47 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let x = 0
-let y = 0
+const heroImg = new Image()
+heroImg.src = '/hero.jpeg'
+function Hero(x, y) {
+    this.x = x
+    this.y = y
+    this.xVel = 0
+    this.yVel = 0
 
-xVel = 0
-yVel = 0
+
+
+    this.animate = function () {
+        this.x = this.x + this.xVel
+        this.y = this.y + this.yVel
+        ctx.drawImage(heroImg, this.x, this.y, 10, 10)
+    }
+}
+
+const hero1 = new Hero(0,0)
 
 window.addEventListener("keyup" , function(event) {
-    xVel = 0
-    yVel = 0
+    hero1.xVel = 0
+    hero1.yVel = 0
 })
 
 window.addEventListener("keydown" , function(event) {
     if (event.key == 'w') {
-        yVel = -1
+        hero1.yVel = -1
     } else if (event.key == 'a'){
-        xVel = -1
+        hero1.xVel = -1
     } else if (event.key == 's'){
-        yVel = 1
+        hero1.yVel = 1
     } else if (event.key == 'd'){
-        xVel = 1
+        hero1.xVel = 1
     }
 })
 
 function someSortOfFunction() {
     void ctx.clearRect(0, 0, 1000, 1000);
 
-    ctx.fillStyle = 'green';
-    x += xVel
-    y += yVel
-    ctx.fillRect(x, y, 10, 10);
+    
+    hero1.animate()
 
     requestAnimationFrame(someSortOfFunction)
 }
