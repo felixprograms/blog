d283c6aabe0807aac29270279458ab2fc480c3b5
felix <felixprograms1205@gmail.com>
2022-01-03 16:57:10 +0800

    commit

--- public/space.js
+++ public/space.js
@@ -3,21 +3,33 @@ const ctx = canvas.getContext('2d');
 
 let x = 0
 let y = 0
+
+xVel = 0
+yVel = 0
+
+window.addEventListener("keyup" , function(event) {
+    xVel = 0
+    yVel = 0
+})
+
 window.addEventListener("keydown" , function(event) {
-    if (event.key == 'ArrowUp') {
-        y -= 1
-    } else if (event.key == 'ArrowLeft'){
-        x -= 1
-    } else if (event.key == 'ArrowDown'){
-        y += 1
-    } else if (event.key == 'ArrowRight'){
-        x += 1
+    if (event.key == 'w') {
+        yVel = -1
+    } else if (event.key == 'a'){
+        xVel = -1
+    } else if (event.key == 's'){
+        yVel = 1
+    } else if (event.key == 'd'){
+        xVel = 1
     }
 })
+
 function someSortOfFunction() {
     void ctx.clearRect(0, 0, 1000, 1000);
 
     ctx.fillStyle = 'green';
+    x += xVel
+    y += yVel
     ctx.fillRect(x, y, 10, 10);
 
     requestAnimationFrame(someSortOfFunction)
