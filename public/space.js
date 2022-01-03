24ee51828c8cad417ea7b98c454d5c236c69ffab
felix <felixprograms1205@gmail.com>
2022-01-03 17:43:10 +0800

    commit

--- public/space.js
+++ public/space.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
+const badImg = new Image()
 const heroImg = new Image()
 heroImg.src = '/hero.jpeg'
+badImg.src = '/alien.jpeg'
 function Hero(x, y) {
     this.x = x
     this.y = y
@@ -18,8 +19,25 @@ function Hero(x, y) {
     }
 }
 
-const hero1 = new Hero(0,0)
-
+function BadGuy(x,y) {
+    this.x = x
+    this.y = y
+    this.xVel = 0
+    this.yVel = 0
+    this.animate = function () {
+        this.x = this.x + this.xVel
+        this.y = this.y + this.yVel
+        ctx.drawImage(badImg, this.x, this.y, 15, 15)
+    }
+}
+const hero1 = new Hero(150,100)
+const aliens = []
+for (let y = 0; y < 3; y += 1) {
+    for (let i = 0; i < 15; i += 1) {
+        const alien = new BadGuy(0 + i * 20, y * 20)
+        aliens.push(alien)
+    }
+}
 window.addEventListener("keyup" , function(event) {
     hero1.xVel = 0
     hero1.yVel = 0
@@ -42,7 +60,9 @@ function someSortOfFunction() {
 
     
     hero1.animate()
-
+    aliens.forEach(alien => {
+        alien.animate()
+    })
     requestAnimationFrame(someSortOfFunction)
 }
 
